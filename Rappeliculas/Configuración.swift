09a3d9b89668de32a6f840b472d3906ec60deedb
Alejandro Agudelo <carlosaguirre7@hotmail.com>
2018-10-28 23:19:56 -0500

    Patrón registro implementado

---
+++ Rappeliculas/Configuración.swift
@@ -0,0 +1,19 @@
+//
+//  Configuración.swift
+//  Rappeliculas
+//
+//  Created by Alejandro Agudelo on 28/10/18.
+//  Copyright © 2018 Alejandro Agudelo. All rights reserved.
+//
+
+import Foundation
+
+struct Registro {
+    private init(){}
+    struct conexionIMDB {
+        let URLservicio = "https://api.themoviedb.org/4/"
+        let tokenAcceso = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Zjc2YjViMTQ5ZDgwZTE2MGM5Y2ZmN2U0MDkzODU2ZSIsInN1YiI6IjU5YTIxYzRhYzNhMzY4NDgxZTAzNDI5ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-HV94_m7TG32kGHvyo7XyUhxCjrGgfgcyMnCo2_u_fw"
+        
+    }
+    
+}
