7c55c783679848931714011da3db195deb67e793
Alejandro Agudelo <carlosaguirre7@hotmail.com>
2018-10-29 23:06:37 -0500

    -Configuración actualizada -Estructura persona implementada

--- Rappeliculas/Configuración.swift
+++ Rappeliculas/Configuración.swift
@@ -11,9 +11,7 @@ import Foundation
 struct Registro {
     private init(){}
     struct conexionIMDB {
-        let URLservicio = "https://api.themoviedb.org/4/"
-        let tokenAcceso = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2Zjc2YjViMTQ5ZDgwZTE2MGM5Y2ZmN2U0MDkzODU2ZSIsInN1YiI6IjU5YTIxYzRhYzNhMzY4NDgxZTAzNDI5ZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-HV94_m7TG32kGHvyo7XyUhxCjrGgfgcyMnCo2_u_fw"
-        
+        static let URLservicio = "https://api.themoviedb.org/4/"
+        static let apiKey = "6f76b5b149d80e160c9cff7e4093856e"
     }
-    
 }
